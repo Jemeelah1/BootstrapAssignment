b5c4daa53c9c0377cdd1a5a3e6d9dd5430f5916a
Jemeelah1 <83028679+Jemeelah1@users.noreply.github.com>
2021-11-25 09:29:15 +0100

    index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
+
+function validateForm() {
+  let x = document.forms["myForm"]["fname"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+}
