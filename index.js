dcd7f15d3ba9356761a1d7a2afb1ff6b00a2adde
Jemeelah1 <83028679+Jemeelah1@users.noreply.github.com>
2021-11-26 12:49:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-var myVar;
+const names=document.querySelector(".form-control");
+const email=document.querySelector(".emailinput");
+const phone=document.querySelector(".phoneinput");
+const subject=document.querySelector(".subjectinput");
+const dob=document.querySelector(".dobinput");
+const text=document.querySelector(".textinput");
 
+var myVar;
 function myFunction() {
   myVar = setTimeout(showPage, 3000);
 }
@@ -9,10 +15,61 @@ function showPage() {
   document.getElementById("myDiv").style.display = "block";
 }
 
-function validateForm() {
-  let x = document.forms["myForm"]["fname"].value;
+function SubscribeForm() {
+  let x = document.querySelector(".subscribe").value;
   if (x == "") {
-    alert("Name must be filled out");
+    document.querySelector(".subscribeForm").innerHTML = "this field must not be empty"
     return false;
   }
 }
+
+function SubscribeFooter() {
+  let x = document.querySelector(".subscribeFooter").value;
+  if (x == "") {
+    document.querySelector(".subFooter").innerHTML = "this field must not be empty"
+    return false;
+  }
+}
+
+function validation(event) {
+  event.preventDefault();
+  if(!names.value) {
+      document.querySelector(".name").innerHTML = "this field must not be empty"
+  } else if (!email.value) {
+      document.querySelector(".email").innerHTML = "enter your email address"
+  } else if (!phone.value) {
+      document.querySelector(".phone").innerHTML = "enter your phone number"
+  } else if (!subject.value) {
+      document.querySelector(".selectsub").innerHTML = "select subject"
+  } else if (!dob.value) {
+      document.querySelector(".dob").innerHTML = "enter date of birth"
+  } else if (!text.value) {
+      document.querySelector(".text").innerHTML = "write a message"
+  } else {
+      console.log("wrong!")
+  }
+}
+
+var section = document.querySelector('.numbers');
+var hasEntered = false;
+
+window.addEventListener('scroll', (e) => {
+	var shouldAnimate = (window.scrollY + window.innerHeight) >= section.offsetTop;
+
+	if (shouldAnimate && !hasEntered) {
+  	hasEntered = true;
+    
+    $('.value').each(function () {
+    	$(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    	}, {
+        duration: 1000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+   		});
+    });
+
+  }
+});
